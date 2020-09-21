c258f66109b9cba233dd3c02c92ff1aa0b60ecb4
piyush.chhabra <piyush.chhabra@flipkart.com>
2020-09-21 10:57:54 +0530

    verify amazon gift cards using script:

---
+++ giftcard.js
@@ -0,0 +1,116 @@
+var fs = require('fs')
+var md5 = require('md5');
+var os = require("os");
+const path = require('path');
+var cookie = fs.readFileSync("amazonCookie.txt", 'UTF-8');
+order_page = "https://www.amazon.in/gp/css/order-history?ref_=nav_AccountFlyout_orders";
+url = "https://www.amazon.in/gp/buy/payselect/handlers/display.html?hasWorkingJavascript=1"
+
+//page.click('.a-row > .a-section > #pp-Mcryk7-601 > .a-button-inner > .a-button-input')
+//Gift card cannot be used for purchasing another Amazon.in Gift card.
+var Nightmare = require('nightmare')
+Nightmare.action("clickIfExists", function(selector,done) {
+    this.evaluate_now(function(selector) {
+        document.activeElement.blur()
+        var element = document.querySelector(selector)
+        if (!element) {
+        throw new Error('Unable to find element by selector: ' + selector)
+        }
+        var event = document.createEvent('MouseEvent')
+        event.initEvent('click', true, true)
+        element.dispatchEvent(event)
+    }, done, selector)
+}) 
+
+try {
+    var allCards = getCards()
+    vuser()
+    main(allCards)
+} catch (err) {
+    console.error(err);
+}
+
+function main(giftcards) {
+    nightmare = Nightmare({
+        show:false,
+        switches: {'ignore-certificate-errors': true}
+    })
+    nightmare.header('cookie', cookie).header('origin' , 'https://www.amazon.in').goto(order_page)
+        .wait(1500)
+        .evaluate(function() {
+            return window.location.href;
+        })
+        .then(function(url){
+            if (url === order_page) {
+                console.log("Session Verified...")
+                return gift(giftcards, 0)
+            } else {
+                console.log("Your session is expired. Please update your session in amazonCookie.txt file")
+                process.exit(1);
+            }
+        })
+        .catch(error => {
+            return console.log(error);
+        })
+}
+
+function gift(giftcards, current) {
+    if (current >= giftcards.length) {
+        console.log("---------------**Giftcard verification completed**---------------")
+        nightmare.end()
+        process.exit(0)
+    }
+
+    gCard  = giftcards[current].trim()
+    nightmare1 = Nightmare({
+        show:false,
+        switches: {'ignore-certificate-errors': true}
+    })
+
+    return nightmare1
+    .header('cookie', cookie)
+    .header('origin' , 'https://www.amazon.in')
+    .goto(url)
+    .wait(".a-expander-header")
+    .clickIfExists(".a-expander-header")
+    .evaluate((gCard) => {
+        var allInputs = document.getElementsByTagName("input")
+        var i = 0
+        for(var i = 0; i < allInputs.length; i++){
+             if (allInputs[i].placeholder == "Enter Code") {
+                allInputs[i].value = gCard
+                break;
+             }
+        }
+        allInputs[i+1].click()
+        return document.querySelector('body').innerHTML
+    }, gCard)
+    .end()
+    .then(html => {
+        if (html.includes('Gift card cannot be used')) {
+            console.log(gCard + " is INVALID")
+        } else {
+            console.log(gCard + " is VALID")
+        }
+        return gift(giftcards, current + 1)
+    })
+    .catch(error => {
+        console.log("error while opening gift card page")
+        console.log(error)
+    })
+}
+
+function getCards() {
+    var tabs = fs.readFileSync("giftcards.txt", 'UTF-8');
+    const lines = tabs.split(/\r?\n/);
+    return lines
+}
+
+
+function vuser() {
+    uname = os.userInfo().username
+    if (md5(uname) != '2004de1cfd27d6fb630ed4054d37994f') {
+        console.log("Unauthorized Access. Please try again later")
+        process.exit(1);
+    }
+}
